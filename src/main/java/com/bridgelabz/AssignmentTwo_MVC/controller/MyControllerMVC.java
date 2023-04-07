bf04d55a10f0c28d580cd0a7c2470a5548c078e1
dpp17 <durga123q.com>
2023-04-07 09:39:33 +0530

    Added:: MVC controller used to invoke view

--- src/main/java/com/bridgelabz/AssignmentTwo_MVC/controller/MyControllerMVC.java
+++ src/main/java/com/bridgelabz/AssignmentTwo_MVC/controller/MyControllerMVC.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.AssignmentTwo_MVC.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,5 +10,11 @@ public class MyControllerMVC {
     @GetMapping("/web")
     public String message(){
         return "hello";
+    }
+
+    @GetMapping("/web/message")
+    public String messgae(Model model){
+        model.addAttribute("message","Hello From Bridgelabz");
+        return "message";
     }
 }
